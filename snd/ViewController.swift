cb9594bfe512e1209b260123aa3cdacaeb5017fb
Никита Казанцев <nikita@mbp-nikita.dlink>
2018-02-17 17:42:56 +0300

    Initial Commit

---
+++ snd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snd
+//
+//  Created by Никита on 17.02.2018.
+//  Copyright © 2018 Никита. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
